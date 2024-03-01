d26454da1b969aa171399e2d7e4378256d1894b1
rajinsharwar2 <rajinsharwar@gmail.com>
2024-02-29 23:56:36 -0500

    Testing unit test

---
+++ test/ActivateTest.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class ActivateTest extends \PHPUnit\Framework\TestCase {
+
+    public function testone(){
+        $instance = new Activate();
+        $ex = 5;
+    
+        self::assertEquals($ex, $a);
+    }
+}
