390693f5e7d163f72bde279779d038c047455486
rajinsharwar2 <rajinsharwar@gmail.com>
2024-03-05 22:19:22 -0500

    Adding more unit tests

--- tests/test-staticserve-class.php
+++ tests/test-staticserve-class.php
@@ -65,7 +65,24 @@ class Test_StaticServe_Class extends WP_UnitTestCase {
             // Delete the directory itself
             rmdir($cache_dir);
         }
-    }       
+    }
+
+    public function is_dir_empty($cache_dir){
+        // Get the list of files and directories in the cache directory
+        $directory_contents = scandir($cache_dir);
+
+        // Remove . and .. from the list of directory contents
+        $directory_contents = array_diff($directory_contents, array('.', '..'));
+
+        // Check if the directory contents array is empty
+        if (empty($directory_contents)) {
+            // The directory is empty
+            return true;
+        } else {
+            // The directory is not empty
+            return false;
+        }
+    }
     
     public function test_build_denied(){
         $_SERVER['HTTP_X_SERVE_STATIC_REQUEST'] = '';
@@ -153,6 +170,7 @@ class Test_StaticServe_Class extends WP_UnitTestCase {
 
         // Unset the mock request header
         unset($_SERVER['HTTP_X_SERVE_STATIC_REQUEST']);
+        unset($_SERVER['REQUEST_URI']);
         delete_option( 'serve_static_exclude_urls' );
         delete_option( 'serve_static_make_static' );
     }
@@ -197,7 +215,156 @@ class Test_StaticServe_Class extends WP_UnitTestCase {
 
         // Unset the mock request header
         unset($_SERVER['HTTP_X_SERVE_STATIC_REQUEST']);
+        unset($_SERVER['REQUEST_URI']);
         delete_option( 'serve_static_exclude_urls' );
         delete_option( 'serve_static_post_types_static' );
     }
+
+    public function test_make_manual_entry_static_denied(){
+
+        // Mock the current URL
+        $_SERVER['HTTP_HOST'] = 'wp-develop.local';
+        $_SERVER['REQUEST_URI'] = '/blog/';
+
+        update_option( 'serve_static_manual_entry', '1' );
+
+        $_SERVER['HTTP_X_SERVE_STATIC_REQUEST'] = 'true';
+    
+        // Update the option with the formatted array
+        update_option('serve_static_urls', array('http://wp-develop.local/' => 1));
+
+        $this->assertFalse($this->static_serve->Build());
+
+        // Unset the mock request header
+        unset($_SERVER['HTTP_X_SERVE_STATIC_REQUEST']);
+        unset($_SERVER['REQUEST_URI']);
+        delete_option( 'serve_static_manual_entry' );
+    }
+
+    public function test_make_manual_entry_static_access(){
+
+        switch_theme( 'twentytwenty' );
+    
+        // Create a new page and set it as the home page
+        $new_page_id = wp_insert_post( array(
+            'post_title'     => 'Your New Page Title',
+            'post_content'   => 'Your page content goes here.',
+            'post_status'    => 'publish',
+            'post_type'      => 'page',
+        ) );
+        update_option( 'page_on_front', $new_page_id );
+        update_option( 'show_on_front', 'page' );
+    
+        // Mock the current URL
+        $site_url = get_site_url();
+    
+        // Remove "http://" from the beginning of the URL using regular expressions
+        $site_url_without_http = preg_replace('/^http?:\/\//i', '', $site_url);
+    
+        // Set the HTTP_HOST using the modified URL
+        $_SERVER['HTTP_HOST'] = $site_url_without_http;
+        $_SERVER['REQUEST_URI'] = '/';
+    
+        update_option( 'serve_static_manual_entry', '1' );
+    
+        $_SERVER['HTTP_X_SERVE_STATIC_REQUEST'] = 'true';
+    
+        // Update the option with the formatted array
+        update_option('serve_static_urls', array($site_url . '/' => 1));
+    
+        $this->assertNull($this->static_serve->Build());
+    
+        $this->clean_up();
+    
+        ob_start();
+        ob_end_flush();
+        ob_get_clean();
+    
+        // Unset the mock request header
+        unset($_SERVER['HTTP_X_SERVE_STATIC_REQUEST']);
+        delete_option( 'serve_static_manual_entry' );
+    }
+
+    public function test_flush_all(){
+        $cache_dir = WP_CONTENT_DIR . '/html-cache';
+        // Ensure the cache directory exists
+        if (!file_exists($cache_dir)) {
+            mkdir($cache_dir, 0755, true);
+        }
+
+        // Set the file path
+        $file_path = $cache_dir . '/new_file.html';
+
+        // Your content to be written to the file
+        $file_content = '<html><body><p>This is the content of the new file.</p></body></html>';
+
+        // Write content to the file
+        $this->assertIsInt(file_put_contents($file_path, $file_content));
+
+        $this->assertFalse($this->is_dir_empty($cache_dir));
+        $this->static_serve->Flush();
+        $this->assertTrue($this->is_dir_empty($cache_dir));
+    }
+
+    public function test_flush_url(){
+        $cache_dir = WP_CONTENT_DIR . '/html-cache/blog';
+        // Ensure the cache directory exists
+        if (!file_exists($cache_dir)) {
+            mkdir($cache_dir, 0755, true);
+        }
+
+        // Set the file path
+        $file_path = $cache_dir . '/index.html';
+
+        // Your content to be written to the file
+        $file_content = '<html><body><p>This is the content of the new file.</p></body></html>';
+
+        // Write content to the file
+        $this->assertIsInt(file_put_contents($file_path, $file_content));
+
+        $this->assertFalse($this->is_dir_empty($cache_dir));
+        $this->static_serve->Flush(get_site_url() . '/blog');
+        $this->assertTrue($this->is_dir_empty($cache_dir));
+    }
+
+    public function test_not_cache_available(){
+        $cache_dir = WP_CONTENT_DIR . '/html-cache';
+        // Ensure the cache directory exists
+        if (!file_exists($cache_dir)) {
+            mkdir($cache_dir, 0755, true);
+        }
+
+        // Set the file path
+        $file_path = $cache_dir . '/index.html';
+
+        // Your content to be written to the file
+        $file_content = '<html><body><p>This is the content of the new file.</p></body></html>';
+
+        // Write content to the file
+        $this->assertIsInt(file_put_contents($file_path, $file_content));
+
+        $this->assertFalse($this->static_serve->is_cache_available(get_site_url() . '/blog'));
+        $this->clean_up();
+    }
+
+    public function test_cache_available(){
+        $cache_dir = WP_CONTENT_DIR . '/html-cache/blog';
+        // Ensure the cache directory exists
+        if (!file_exists($cache_dir)) {
+            mkdir($cache_dir, 0755, true);
+        }
+
+        // Set the file path
+        $file_path = $cache_dir . '/index.html';
+
+        // Your content to be written to the file
+        $file_content = '<html><body><p>This is the content of the new file.</p></body></html>';
+
+        // Write content to the file
+        $this->assertIsInt(file_put_contents($file_path, $file_content));
+
+        $this->assertTrue($this->static_serve->is_cache_available(get_site_url() . '/blog'));
+        $this->clean_up();
+    }
+    
 }
