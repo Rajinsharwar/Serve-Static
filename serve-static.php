f56fe76c0ea0943fa83879bd15f838e03e3ddbb3
rajinsharwar2 <rajinsharwar@gmail.com>
2024-03-09 00:37:24 -0500

    analytics fix

--- serve-static.php
+++ serve-static.php
@@ -35,7 +35,7 @@ if ( ! function_exists( 'serve_static_analytics' ) ) {
                 'has_addons'          => false,
                 'has_paid_plans'      => false,
                 'menu'                => array(
-                    'slug'           => 'serve_static_settings',
+                    'slug'           => 'serve_static_guide',
                     'first-path'     => 'admin.php?page=serve_static_guide',
                     'account'        => false,
                 ),
